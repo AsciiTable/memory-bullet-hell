e9c4b8b96427ed5695059a95fdd3aeb004cda8f1
JustinCheung6 <justincheung6@gmail.com>
2020-05-16 01:44:45 -0700

    -Added Main Menu (WIP)

---
+++ Memory Bullet Hell/Assets/Scripts/MenuScreens/MenuScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    /*
+     * This script depends on the scenes being in a specific order:
+     * 0: Main Menu
+     * 1: Game Scene
+     */
+    public void GoToGame()
+    {
+
+        if (1 < SceneManager.sceneCountInBuildSettings)
+        {
+            Debug.Log("Going to Game Scene");
+            SceneManager.LoadScene(1);
+        }
+        else
+        {
+            Debug.Log("Game Scene not Found");
+        }
+    }
+
+    public void GoToTitle()
+    {
+        if (0 < SceneManager.sceneCountInBuildSettings)
+        {
+            Debug.Log("Going to Title Scene");
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            Debug.Log("Title Scene not Found");
+        }
+    }
+
+    public void ExitApplication()
+    {
+        Debug.Log("Stopping Application");
+        Application.Quit();
+    }
+}
---
+++ Memory Bullet Hell/Assets/Scripts/MenuScreens/PauseMenuScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuScript : MenuScript
+{
+    /*
+    * This script depends on the scenes being in a specific order:
+    * 0: Main Menu
+    * 1: Game Scene
+    */
+    private Canvas pauseMenu = null;
+    private bool paused = false;
+
+    private void OnEnable()
+    {
+        UpdateHandler.UpdateOccurred += CheckPaused;
+    }
+    private void OnDisable()
+    {
+        UpdateHandler.UpdateOccurred -= CheckPaused;
+    }
+
+    private void Start()
+    {
+        pauseMenu = GetComponentInChildren<Canvas>();
+        pauseMenu.gameObject.SetActive(false);
+    }
+    public void CheckPaused()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            if (!paused)
+                PauseGame();
+            else
+                ResumeGame();
+        }
+    }
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        pauseMenu.gameObject.SetActive(true);
+        paused = true;
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        pauseMenu.gameObject.SetActive(false);
+        paused = false;
+    }
+}
