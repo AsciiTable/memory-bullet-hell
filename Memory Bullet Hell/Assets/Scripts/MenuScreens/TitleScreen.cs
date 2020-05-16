89e357226b811245c3b6a984e8544c95b62f7eb1
JustinCheung6 <justincheung6@gmail.com>
2020-05-15 22:15:14 -0700

    Menu Done

---
+++ Memory Bullet Hell/Assets/Scripts/MenuScreens/TitleScreen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+    
+
+    public void PlayGame()
+    {
+        int index = SceneManager.GetActiveScene().buildIndex + 1;
+
+        if (index < SceneManager.sceneCountInBuildSettings)
+        {
+            Debug.Log("Going to Game Scene");
+            SceneManager.LoadScene(index);
+        }
+        else
+        {
+            Debug.Log("Game Scene not Found");
+        }
+    }
+
+    public void ExitApplication()
+    {
+        Debug.Log("Stopping Application");
+        Application.Quit();
+    }
+}
