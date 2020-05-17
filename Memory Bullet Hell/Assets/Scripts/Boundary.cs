da707b66ad1febf67da1cbca626c22566af4bf50
Jessica Wei <jessicawei9@gmail.com>
2020-05-16 20:21:02 -0700

    Boundary for player implemented

--- Memory Bullet Hell/Assets/Scripts/Boundary.cs
+++ Memory Bullet Hell/Assets/Scripts/Boundary.cs
@@ -4,32 +4,25 @@ using UnityEngine;
 
 public class Boundary : MonoBehaviour
 {
+    public Camera MainCamera;
     private Vector2 screenBounds;
     private float objectWidth;
     private float objectHeight;
 
-    // Start is called before the first frame update
-    private void Start()
+    // Use this for initialization
+    void Start()
     {
-        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
-        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
-        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
+        screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x; //extents = size of width / 2
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y; //extents = size of height / 2
     }
 
-/*    private void OnEnable()
+    // Update is called once per frame
+    void LateUpdate()
     {
-        UpdateHandler.UpdateOccurred += KeepTrackOfBorder;
-    }
-
-    private void OnDisable()
-    {
-        UpdateHandler.UpdateOccurred -= KeepTrackOfBorder;
-    }*/
-
-    private void KeepTrackOfBorder() {
         Vector3 viewPos = transform.position;
-        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x + objectWidth, screenBounds.x * -1 - objectWidth);
-        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y + objectHeight, screenBounds.y * -1 - objectHeight);
+        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1 + objectWidth, screenBounds.x - objectWidth);
+        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y * -1 + objectHeight, screenBounds.y - objectHeight);
         transform.position = viewPos;
     }
 }
