41cfd873a6a31eaf0a534719e3fd9cee641aa19f
Jessica Wei <jessicawei9@gmail.com>
2020-05-16 17:39:00 -0700

    bullet pong has transparncy added

--- Memory Bullet Hell/Assets/Scripts/BulletPong.cs
+++ Memory Bullet Hell/Assets/Scripts/BulletPong.cs
@@ -7,11 +7,17 @@ public class BulletPong : BulletBouncy
     [SerializeField] List<Vector2> newPongDirections;
     private int numOfBouncesLeft;
     private Rigidbody2D rb;
+    private SpriteRenderer sprt;
+    private float transparancyDecrement;
+    private float transparancy;
 
     protected override void OnEnable()
     {
+        sprt = this.gameObject.GetComponent<SpriteRenderer>();
         rb = this.gameObject.GetComponent<Rigidbody2D>();
         numOfBouncesLeft = newPongDirections.Count;
+        transparancyDecrement = 1.0f / (float)numOfBouncesLeft;
+        transparancy = 1;
         BouncyBulletThrust(initialThrustX, initialThrustY, rb);
     }
 
@@ -24,6 +30,8 @@ public class BulletPong : BulletBouncy
             gameObject.SetActive(false);
         }
         else {
+            transparancy = transparancy - transparancyDecrement;
+            sprt.color = new Color(sprt.color.r, sprt.color.g, sprt.color.b, transparancy);
             BouncyBulletThrust(newPongDirections[newPongDirections.Count - numOfBouncesLeft].x, newPongDirections[newPongDirections.Count - numOfBouncesLeft].y, rb);
             numOfBouncesLeft--;
         }
