3b8982cfa54bf308390dbe3ce897c7270dd030c1
Jessica Wei <jessicawei9@gmail.com>
2020-05-17 12:37:35 -0700

    pushing some art implementation

--- Memory Bullet Hell/Assets/Scripts/BulletColumn.cs
+++ Memory Bullet Hell/Assets/Scripts/BulletColumn.cs
@@ -21,6 +21,7 @@ public class BulletColumn : BulletMovement
         columnCol = gameObject.GetComponent<Collider2D>();
         sprt = gameObject.GetComponent<SpriteRenderer>();
         columnCol.enabled = false;
+        Debug.Log("Column Active Time: " + columnActiveTime);
         StartCoroutine(SetUpTakeDown());
         UpdateHandler.UpdateOccurred += bulletMoveLeft;
     }
@@ -31,12 +32,16 @@ public class BulletColumn : BulletMovement
     }
 
     private IEnumerator SetUpTakeDown() {
+        Debug.Log("Column Active Time: " + columnActiveTime);
         sprt.color = new Color(sprt.color.r, sprt.color.g, sprt.color.b, 0.5f);
         yield return new WaitForSeconds(preColumnTime);
+        Debug.Log("Column Active Time: " + columnActiveTime);
         columnCol.enabled = true;
         sprt.color = new Color(sprt.color.r, sprt.color.g, sprt.color.b, 1f);
         currTime = Time.time;
+        Debug.Log("Column Active Time: " + columnActiveTime);
         yield return new WaitForSeconds(columnActiveTime);
+        Debug.Log("Column Active Time: " + columnActiveTime);
         sprt.color = new Color(sprt.color.r, sprt.color.g, sprt.color.b, 0.2f);
         columnCol.enabled = false;
         yield return new WaitForSeconds(postColumnTime);
